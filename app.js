7483aaba8e8e4be6799819a7ed93dfc0767703f1
Isaque <isaquecoelho.dj@gmail.com>
2026-05-12 20:49:46 -0300

    feat: adiciona rotas para dentistas

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const dentistaRoutes = require("./routes/dentista");
 app.use(express.json());
+app.use("/dentistas", dentistaRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
